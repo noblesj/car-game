d92e7c0d9f334533a75fb284728c15d0bb6222df
noblesj <143807497+noblesj@users.noreply.github.com>
2024-03-28 12:09:22 -0400

    what ever i have
    e

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -6,3 +6,26 @@
  * Adds the game compenents ( like the race track) to the window.
  * other methods if necessary
  */
+import javax.swing.*;
+
+public class Gamewindow extends JFrame {
+    // start ui
+    public CarRacingGameGUI() {
+        super("Car Racing Game");
+        maxNumberOfCars = getMaxNumberOfCarsFromUser();
+        carIcons = new ArrayList<>();
+        initializeRace();
+        createGUI();
+    }
+    //get number of cars from the user
+    private int getMaxNumberOfCarsFromUser() {
+        String input = JOptionPane.showInputDialog(null, "Enter maximum number of cars:");
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Invalid input. Defaulting to 2 cars.");
+            return 2; // Default to 2 cars if input is invalid
+        }
+    }
+    private void initializeRace() {
+}
