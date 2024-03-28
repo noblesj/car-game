7afbd43b189f1c0738e1ecca22c1c57a35fd599e
Ramiz Guliyev <105210518+RamizzGuliyev@users.noreply.github.com>
2024-03-28 11:57:02 -0400

    Initialized classes and commented some of them for insturction.

---
+++ src/GameLogic.java
@@ -0,0 +1,6 @@
+/**
+ * This class should manage game logic such as initializing the race.
+ * startRace()
+ * checkWinner()
+ * others if neccessary
+ */
---
+++ src/GameWindow.java
@@ -0,0 +1,8 @@
+/**
+ * Set up application window here, and display the game
+ * intitialize()
+ * This method sets up the window properties ( size, title, and etc.. )
+ * addComponents()
+ * Adds the game compenents ( like the race track) to the window.
+ * other methods if necessary
+ */
